d216b32b1fe386d604f09bcaf1d2281875b172dd
gabocode2907 <83044398+gabocode2907@users.noreply.github.com>
2021-06-16 21:13:57 -0500

    update

--- dojo_reads/main/models.py
+++ dojo_reads/main/models.py
@@ -10,6 +10,14 @@ class UserManager(models.Manager):
         errors = {}
         if len(postData['name']) < 2:
             errors['name'] = "Invalid Name. Name must be at least 3 characters"
+        if len(postData['alias']) < 2:
+            errors['alias'] = "Invalid Alias. Alias must be at least 3 characters"
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Ivalid Email"
+        if len(postData['password']) < 5:
+            errors['passwrod'] = "Password must be at leaset 6 characters"
+        if postData['password'] != postData['confirm_password']:
+            errors['pw_match'] = "Password does not match"
 
 class User(models.Model):
     name = models.CharField(max_length=50)
